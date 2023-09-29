c6962a7347e3468f9f380f7083fda1ed5d6b2322
brunosellas <bruno_sellas@hotmail.com>
2023-09-28 22:27:25 -0300

    Adicionando os erros da classes de retorno Errors

--- dev/src/Catalog/IngressoFacil.Catalog.API/Errors/SessionErrors.cs
+++ dev/src/Catalog/IngressoFacil.Catalog.API/Errors/SessionErrors.cs
@@ -2,14 +2,14 @@
 
 namespace IngressoFacil.Catalog.API.Errors {
     public class SessionStartTimeIsNotValidError : Error {
-        public SessionStartTimeIsNotValidError() : base() { }
+        public SessionStartTimeIsNotValidError(TimeSpan time) : base($"O tempo de inicio da sessão: {time} não é valido.") { }
     } 
 
     public class SessionNotFoundError : Error {
-
+        public SessionNotFoundError() : base("A sessão não pode ser encontrada.") { }
     }
 
     public class SessionDateBeforeTodayError : Error {
-
+        public SessionDateBeforeTodayError(DateOnly date) : base($"A sessão não pode ser criada na data : {date}.") { }
     }
 }
