6f1b22ded9000e06acf0213cba759654aa4520f1
brunosellas <bruno_sellas@hotmail.com>
2023-10-01 12:56:51 -0300

    Polindo e inserindo argumentos no retorno dos erros

--- dev/src/Catalog/IngressoFacil.Catalog.API/Errors/MovieErrors.cs
+++ dev/src/Catalog/IngressoFacil.Catalog.API/Errors/MovieErrors.cs
@@ -1,6 +1,10 @@
 ﻿using FluentResults;
 
 namespace IngressoFacil.Catalog.API.Errors {
-    public class MovieAlreadyExistsError : Error {}
-    public class MovieNotFoundError : Error {}
+    public class MovieAlreadyExistsError : Error {
+    public MovieAlreadyExistsError(string MovieAlreadyExists) : base($"O filme {MovieAlreadyExists} já existe.") { }
+    }
+    public class MovieNotFoundError : Error {
+    public MovieNotFoundError(string MovieNotFound) :base($"O filme {MovieNotFound} não pode ser encontrado.") { }
+    }
 }
--- dev/src/Catalog/IngressoFacil.Catalog.API/Errors/TheaterErrors.cs
+++ dev/src/Catalog/IngressoFacil.Catalog.API/Errors/TheaterErrors.cs
@@ -1,7 +1,21 @@
 ﻿using FluentResults;
 
 namespace IngressoFacil.Catalog.API.Errors {
-    public class TheaterNumberIsNegativeError : Error {}
-    public class TheaterNotFoundError : Error {}
-    public class TheaterAlreadyExistsError : Error {}
+    public class TheaterNumberIsNegativeError : Error 
+    {
+
+    public TheaterNumberIsNegativeError(int RoomIsNegative) : base($"O número da sala {RoomIsNegative} não pode ser negativo.") { }
+
+    }
+
+    public class TheaterNotFoundError : Error 
+    {
+
+    public TheaterNotFoundError(int RoomNumberNotFound) : base($"A sala de número {RoomNumberNotFound} não pode ser encontrada.") { }
+    }
+
+    public class TheaterAlreadyExistsError : Error 
+    {
+        public TheaterAlreadyExistsError(int RoomNumberAlreadyExists) : base($"A sala de número {RoomNumberAlreadyExists} já existe.") { }
+    }
 }
